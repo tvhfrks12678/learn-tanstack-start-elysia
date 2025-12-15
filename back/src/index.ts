8455ff590c540995bb335d1dc681d40956cf1538
tvhfrks12678 <tvhfrks12678@gmail.com>
2025-12-15 14:44:55 +0900

    back(build): Update default dev from 3000 to 4000

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const DEFAULT_PORT = 3000;
+
+const port = process.env.PORT ?? DEFAULT_PORT;
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(port);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
