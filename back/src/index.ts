ad446fead917498075582ea38a558795e478f8b4
tvhfrks12678 <tvhfrks12678@gmail.com>
2025-12-25 14:24:24 +0900

    refactor(back): improve port configuration using Effect and Schema

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,4 +1,4 @@
-import { Effect, Schema } from "effect";
+import { Effect, Schema, Option } from "effect";
 import { Elysia } from "elysia";
 
 const DEFAULT_PORT = 3000;
@@ -8,13 +8,33 @@ const PortSchema = Schema.NumberFromString.pipe(
   Schema.between(1, 65535),
 )
 
-const rawPort = process.env.PORT;
+const getPort = Effect.gen(function* () {
+  const rawPort = Option.fromNullable(process.env.PORT)
 
-const portEffect = rawPort
-  ? Schema.decode(PortSchema)(rawPort)
-  : Effect.succeed(DEFAULT_PORT);
+  return yield* Option.match(rawPort, {
+    onNone: () => Effect.succeed(DEFAULT_PORT),
+    onSome: (value) => Schema.decode(PortSchema)(value),
+  })
+})
 
-const port = Effect.runSync(portEffect)
+const port = Effect.runSync(getPort)
+
+// const portEffect = Option.fromNullable(process.env.PORT).pipe(
+//   Option.match({
+//     onNone: () => Effect.succeed(DEFAULT_PORT),
+//     onSome: (value) => Schema.decodeUnknown(PortSchema)(value),
+//   })
+// );
+
+// const port = Effect.runSync(portEffect)
+
+// const rawPort = process.env.PORT;
+
+// const portEffect = rawPort
+//   ? Schema.decode(PortSchema)(rawPort)
+//   : Effect.succeed(DEFAULT_PORT);
+
+// const port = Effect.runSync(portEffect)
 // const port = process.env.PORT ?? DEFAULT_PORT;
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(port);
